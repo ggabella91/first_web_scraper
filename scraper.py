b26c403a636139f6ea23e698b984425a890e8790
Giuliano <ggabella91@gmail.com>
2020-05-16 14:20:25 -0700

    First commit

---
+++ scraper.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+def quote_scraper():
+    url = "http://quotes.toscrape.com"
+    quote_list = []
+    next_link = url
+
+    while True:
+        response = requests.get(next_link)
+        soup = BeautifulSoup(response.text, "html.parser")
+        quotes = soup.find_all("div", class_ = "quote")
+        for quote in quotes:
+            text = quote.find("span", class_ = "text").text
+            print(text)
+
+        next_page = soup.find("li", class_ = "next")
+        
+        if next_page:
+            next_path = next_page.find("a")["href"]
+            next_link = url + next_path
+            print(next_link)
+        else:
+            break
+
+
+quote_scraper()
